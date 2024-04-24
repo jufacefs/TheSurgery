e13cdc082a8430c08b5e7f0566bce9fa13870338
Dororo <hz86569p@pace.edu>
2024-04-24 05:00:08 -0400

    Update

--- ExplorationGame2D-main/Assets/scirpts/Add/ParentObjectControl.cs
+++ ExplorationGame2D-main/Assets/scirpts/Add/ParentObjectControl.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class ParentObjectControl : MonoBehaviour
 {
     public GameObject[] children;
+    public GameObject itemPrefab; // 用于生成的子GameObject的预制件
+    public string[] spritePaths = new string[] { "Sprites/cone", "Sprites/mySprite2" }; // 存放Sprite路径的数组
+    public int maxItems = 5; // 最多生成的项目数量
     private Collider2D parentCollider;
+
+    public List<GameObject> generatedObjects = new List<GameObject>();
+
 
     void Start()
     {
@@ -28,57 +34,74 @@ public class ParentObjectControl : MonoBehaviour
     {
         if (collider == parentCollider)
         {
-            ValueManager.Instance.IncrementValue(); // 当点击父对象时增加数值
-                                                    // 根据 currentValue 决定是否启用子对象的 Collider
+            ValueManager.Instance.IncrementValue();
             ToggleChildrenColliders(ValueManager.Instance.currentValue >= 2);
+            GenerateRandomSprites(); // 在点击时生成新的子对象
             Debug.Log("Parent clicked, children collider state updated based on currentValue.");
         }
         else
         {
-            // 遍历所有子对象，查看是否点击了其中的某一个
-            foreach (GameObject child in children)
+            CheckChildrenColliders(collider);
+        }
+    }
+
+    void CheckChildrenColliders(Collider2D collider)
+    {
+        foreach (GameObject child in children)
+        {
+            Collider2D childCollider = child.GetComponent<Collider2D>();
+            if (collider == childCollider && childCollider.enabled)
             {
-                Collider2D childCollider = child.GetComponent<Collider2D>();
-                if (collider == childCollider && childCollider.enabled)
-                {
-                    Debug.Log(child.name + " child clicked.");
-                    PerformChildAction(child); // 调用一个方法来处理子对象的点击
-                    break; // 找到被点击的子对象后不需要继续遍历
-                }
+                Debug.Log(child.name + " child clicked.");
+                PerformChildAction(child);
+                break;
             }
         }
-        // 检查是否点击了任何注册的 Collider
-        bool anyColliderHit = collider != null;
-
-        // 逻辑处理部分...
-
-        // 如果点击的区域不是任何注册的 Collider
-        if (!anyColliderHit)
-        {
-            ValueManager.Instance.ResetValue();
-            ToggleChildrenColliders(false);
-            Debug.Log("Clicked outside any recognized collider, reset currentValue and disabled all child colliders.");
-        }
     }
 
-    private void PerformChildAction(GameObject child)
+    void PerformChildAction(GameObject child)
     {
-        // 这里可以添加与子对象相关的特定操作
-        // 例如改变子对象的颜色、播放动画、启动游戏逻辑等
-        child.GetComponent<SpriteRenderer>().color = Color.red; // 示例：改变颜色为红色
+        child.GetComponent<SpriteRenderer>().color = Color.red;
     }
-
 
     public void ToggleChildrenColliders(bool enable)
     {
         foreach (GameObject child in children)
         {
             Collider2D childCollider = child.GetComponent<Collider2D>();
-            if (childCollider != null)
+            childCollider.enabled = enable;
+        }
+    }
+
+    void GenerateRandomSprites()
+    {
+        for (int i = 0; i < maxItems; i++)
+        {
+            GameObject newItem = Instantiate(itemPrefab, transform);
+            newItem.transform.localPosition = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+
+            // 随机选择一个Sprite
+            int spriteIndex = Random.Range(0, spritePaths.Length);
+            Sprite newSprite = Resources.Load<Sprite>(spritePaths[spriteIndex]);
+            Debug.Log("Attempting to load sprite from path: " + spritePaths[spriteIndex]);
+            if (newSprite == null)
             {
-                // 根据传入的 enable 参数激活或禁用 Collider
-                childCollider.enabled = enable;
+                Debug.LogError("Failed to load sprite. Check path and sprite settings.");
             }
+            else
+            {
+                newItem.GetComponent<SpriteRenderer>().sprite = newSprite;
+            }
+        }
+    }
+
+
+    public void HideGeneratedObjects()
+    {
+        Debug.Log("Hiding " + generatedObjects.Count + " objects.");
+        foreach (GameObject obj in generatedObjects)
+        {
+            obj.SetActive(false);
         }
     }
 
